a4ac86b45d7f98aa5ff31fc7f0b33b399a689bec
Carolina Aoki <carol.junko@gmail.com>
2026-06-12 15:53:01 +0200

    add print_hello function

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
-
 /* my first c project */
 
 #include <unistd.h>
 
+void print_hello(void)
+{
+	write(1, "Hello, Carolina\n", 17);
+}
+
 int	main(void)
 {
-	write(1, "Hello, Carolina!\n", 17);
+	print_hello();
 	return (0);
 }
+
